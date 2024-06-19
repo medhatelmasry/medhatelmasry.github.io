6c38745e1a11a0c37a325610bb6fff75191771ee
Medhat Elmasry <medhat@elmasry.ca>
2024-06-19 14:44:08 -0700

    surrey school district

--- src/pages/Church.tsx
+++ src/pages/Church.tsx
@@ -7,66 +7,63 @@ const ChurchPage = () => {
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=UVIrNU5yvAo"
-              >A Wellspring of Humility - Saint Nektarios of Aegina</a
-            >
+              >A Wellspring of Humility - Saint Nektarios of Aegina</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=FDxL5_UHiyk"
-              >St. George Coptic Orthodox Church Short Documentary (2020)</a
-            >
+              >St. George Coptic Orthodox Church Short Documentary (2020)</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=FBJk1p5ZbEY"
               >St. George Coptic Orthodox Church in Vancouver destroyed by fire
-              on July 19, 2021 (2021)</a
-            >
+              on July 19, 2021 (2021)</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=ouEfqnuNkBg"
               >Destruction of St. George Coptic Orthodox Church in Surrey, BC,
-              on July 19, 2021 (2022)</a
-            >
+              on July 19, 2021 (2022)</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://square.saic.edu/s/article/Send-email-from-a-Google-Group-address"
-              >How Do I Send Email From A Google Group Address?</a
-            >
+              >How Do I Send Email From A Google Group Address?</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="http://www.fathermakary.net/"
-              >Father Makary</a
-            >
+              >Father Makary</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.logoschannel.com/"
-              >Logos</a
-            >
+              >Logos</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=-oTO35Eh5SQ&list=PLC-4wHwNowg8pIELg467HXG74TgjmdELE"
-              >Pope Kirolos</a
-            >
+              >Pope Kirolos</a>
           </li>
           <li>
             <a
               className="text-decoration-none link-secondary"
               href="https://www.youtube.com/watch?v=g0SeZuuGpC0&list=PLC-4wHwNowg8pIELg467HXG74TgjmdELE&index=1"
-              >Fr. Abdel Messih el Habashi, The Ethiopian Monk</a
-            >
+              >Fr. Abdel Messih el Habashi, The Ethiopian Monk</a>
+          </li>
+          <li>
+            <a
+              className="text-decoration-none link-secondary"
+              href="https://surreyschools.ebasefm.com/rentals"
+              >Surrey Schools Rentals</a>
           </li>
         </ul>
 

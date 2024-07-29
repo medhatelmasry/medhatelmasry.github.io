dff312152998140d6400b2038a55b197e49d7b5a
Medhat Elmasry <medhat@elmasry.ca>
2024-07-28 19:18:37 -0700

    hr in li

---
+++ src/pages/SchoolDistricts.tsx
@@ -0,0 +1,41 @@
+const SchoolDistricts = () => {
+  return (
+    <section>
+        <h1 className="text-center">School Districts</h1>
+        <ul>
+          <li>
+            <a
+              className="text-decoration-none link-secondary"
+              href="https://www.surreyschools.ca/page/30/find-a-school">
+                Map view of schools in Surrey
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-decoration-none link-secondary"
+              href="https://www.sd35.bc.ca/schools/school-map/">
+                Langley School Map
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-decoration-none link-secondary"
+              href="https://www.sd42.ca/schools/">
+                Maple Ridge Schools
+            </a>
+          </li>
+          <li><hr /></li>
+          <li>
+            <a
+              className="text-decoration-none link-secondary"
+              href="https://surreyschools.ebasefm.com/rentals">
+                Surrey Schools Rentals
+            </a>
+          </li>
+
+        </ul>
+
+    </section>
+  );
+};
+export default SchoolDistricts;

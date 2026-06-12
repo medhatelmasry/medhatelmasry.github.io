45ee9d9a8de43e895ea73dd81bd08fd2d3802a3e
Medhat Elmasry <medhat@elmasry.ca>
2026-06-12 16:30:00 -0700

    more links in AI

--- src/pages/AI.tsx
+++ src/pages/AI.tsx
@@ -1,30 +1,38 @@
 const AiPage = () => {
   return (
     <section>
-        <h3 className="text-center">AI</h3>
-        <ul>
-          <li>
-            <a
-              className="text-decoration-none link-secondary"
-              href="https://learn.microsoft.com/en-us/copilot/"
-              >Microsoft Copilot Learning Hub</a>
-          </li>
-          <li>
-            <a
-              className="text-decoration-none link-secondary"
-              href="https://microsoft.github.io/generative-ai-for-beginners/#/"
-              >Generative AI for Beginners</a>
-          </li>
-          <li>
-            <a className="text-decoration-none link-secondary"
-              href="https://copilot.microsoft.com/">copilot.microsoft.com</a>
-          </li>
+      <h3 className="text-center">AI</h3>
+      <ul>
+        <li>
+          <a
+            className="text-decoration-none link-secondary"
+            href="https://learn.microsoft.com/en-us/copilot/"
+          >Microsoft Copilot Learning Hub</a>
+        </li>
+        <li>
+          <a
+            className="text-decoration-none link-secondary"
+            href="https://microsoft.github.io/generative-ai-for-beginners/#/"
+          >Generative AI for Beginners</a>
+        </li>
+        <li>
+          <a className="text-decoration-none link-secondary"
+            href="https://copilot.microsoft.com/">copilot.microsoft.com</a>
+        </li>
 
-          <li>
-            <a className="text-decoration-none link-secondary"
-              href="https://www.youtube.com/watch?v=79-bApI3GIU">Ex-OpenAI Scientist WARNS: "You Have No Idea What is Coming"</a>
-          </li>
-        </ul>
+        <li>
+          <a className="text-decoration-none link-secondary"
+            href="https://www.youtube.com/watch?v=79-bApI3GIU">Ex-OpenAI Scientist WARNS: "You Have No Idea What is Coming"</a>
+        </li>
+        <li>
+          <a className="text-decoration-none link-secondary"
+            href="https://github.com/github/awesome-copilot">🤖 Awesome GitHub Copilot"</a>
+        </li>
+        <li>
+          <a className="text-decoration-none link-secondary"
+            href="https://www.codeitwithai.com/">Code it with AI"</a>
+        </li>
+      </ul>
     </section>
   );
 };
